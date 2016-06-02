4919d5001fededccfdb9a200eb3b35f52e2904e1
Gaëtan DUHAMEL <gaetan.duhamel@plante-et-cite.fr>
2016-06-02 15:16:47 +0200

    Defining Specimen model

--- docs/specimenModel.rb
+++ docs/specimenModel.rb
@@ -5,9 +5,11 @@ genus_ref:integer
 species_ref:integer
 genus:string
 species:string
+taxon:string
+scan_num:string
+sheet: string
 record_date_note:string
 author:string
-scan_num:string
 subspecies:string
 specimen_count:integer
 material_organ:string
@@ -18,7 +20,8 @@ province:string
 municipality:string
 locality:string
 sampling_location:string
+latitude: float
+longitude: float
 habitat:string
 remarks_1:string
 remarks_2:string
-taxon:string
